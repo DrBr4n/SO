0ab5e20fc27bda6ec682cf9442edc8598082dfae
DrBr4n <brunocunhasilva2001@gmail.com>
2021-05-27 17:08:25 +0100

    projeto

---
+++ Projeto/grupo-zz/src/aurras.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Aurras done\n");
+    printf("Aurras done\n");
+
+    return 0;
+}
---
+++ Projeto/grupo-zz/src/aurrasd.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Aurrasd done\n");
+
+    return 0;
+}
